7b07e8747014bd6cc6c7436cda608c7e1809c57d
MANALI TANNA <manalitanna29@gmail.com>
2023-10-07 22:45:51 -0400

    Completed part 2 of tutorial

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [
@@ -72,6 +73,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+# if using any other database change HERE - 'django.db.backends.postgresql', 'django.db.backends.mysql', or 'django.db.backends.oracle'
 
 DATABASES = {
     'default': {
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
